65100603d7ca68779bbae8891b330e74b687484a
HRM <hricoma@eafit.edu.co>
2023-03-07 23:20:07 -0500

    Example for and if

--- main.go
+++ main.go
@@ -5,4 +5,43 @@ import "fmt"
 func main() {
 	greetin := "Hello there, my first code of GO :D"
 	fmt.Print(greetin)
+
+	// slide
+	numbers := []int{3, 4}
+
+	numbers = append(numbers, 6, 7)
+	fmt.Print(numbers)
+
+	numbers = numbers[3:] // take a range of numbers
+	fmt.Print(numbers)
+
+	// for en go, this is the only one that exists
+	// Example 1
+	for i := range numbers {
+		fmt.Print(numbers[i])
+	}
+	// Example 2
+	for i, number := range numbers {
+		fmt.Print("Index: ", i, "Value: ", number)
+	}
+	// Example 3
+	for _, number := range numbers {
+		fmt.Print("Value: ", number)
+	}
+
+	fruits := []string{
+		"Manzana",
+		"Banano",
+		"Sandia",
+		"Melon",
+	}
+
+	for _, fruit := range fruits {
+		if fruit != "Manzana" {
+			continue
+		}
+
+		fmt.Print("Fruit: ", fruit)
+	}
+
 }
